34078f80c359ef1787b055c84c079945d304845d
Nyloner <longni@hustunique.com>
2016-04-22 19:33:38 +0800

    new

--- www.airbnb.com/userinfor.py
+++ www.airbnb.com/userinfor.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/44.0',
@@ -10,7 +12,8 @@ headers = {
 
 def infor(url):
     html=requests.get(url,headers=headers).text
-    parser(html)
+    result=parser(html)
+    return result
 
 def parser(html):
     Verifiedlabels=['Email address','Facebook','LinkedIn','Offline ID']
@@ -38,16 +41,52 @@ def parser(html):
     location=top.find('a').get_text().replace('  ','')
     jointime=top.find('span',attrs={'class':'text-normal'}).get_text().replace('  ','')
     data_mystique_key=left.find('div',attrs={'data-mystique-key':'user_profile_badgesbundlejs'}).find_all('div',attrs={'class':'badge-container space-top-4'})
-    ReviewsLabels=['Verified','Reference','Superhost']
+    ReviewsLabels=['Verified','Reference','Superhost','Reviews']
     ReviewsList={}
     for item in data_mystique_key:
         for key in ReviewsLabels:
             if key in item.get('data-reactid'):
                 ReviewsList[key]=item.get_text().replace('  ','')
-    reviews=left.find('div',id='reviews').find('div',attrs={'class':['reviews_section','as_guest','row-space-top-3']}).find('div',attrs={'class':'reviews'}).find_all('div',attrs={'class':'row'})
-    reviews_as_guest=len(reviews)/2
-    baseinfor={}
-    baseinfor['name']=
-    print(VerifiedList,AboutMeList,ReviewsList)
+    try:
+        reviews=left.find('div',id='reviews').find('div',attrs={'class':['as_guest']}).find('div',attrs={'class':'reviews'}).find_all('div',attrs={'class':'avatar-wrapper'})
+        reviews_as_guest=len(reviews)
+    except:
+        reviews_as_guest='--'
+    result=name+'||'+location+'||'+jointime
+    for key in Verifiedlabels:
+        try:
+            result+='||'+VerifiedList[key]
+        except:
+            result+='||--'
+    for key in AboutMeLabels:
+        try:
+            result+='||'+AboutMeList[key]
+        except:
+            result+='||--'
+    for key in ReviewsLabels:
+        try:
+            result+='||'+ReviewsList[key]
+        except:
+            result+='||--'
+    result+='||'+str(reviews_as_guest)
+    return result.replace('\n','')
 
-infor('https://www.airbnb.com/users/show/2268502')
+def main():
+    userfailed=open('userfailed.txt','a')
+    userdata=open('userdata.txt','a')
+    for line in open('urls.txt','r'):
+        line=line.replace('\n','')
+        url='https://www.airbnb.com'+line.split('||')[-1]
+        try:
+            result=infor(url)
+        except:
+            userfailed.write(line+'\n')
+            time.sleep(2)
+            continue
+        userdata.write(line+'||'+result+'\n')
+        print(line,'--ok')
+        time.sleep(2)
+    userdata.close()
+    userfailed.close()
+
+main()
