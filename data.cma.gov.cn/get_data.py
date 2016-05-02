82faddaebb56e6338967ef3e1c334e1191340a2f
Nyloner <longni@hustunique.com>
2016-05-02 11:08:11 +0800

    update

--- data.cma.gov.cn/get_data.py
+++ data.cma.gov.cn/get_data.py
@@ -6,42 +6,100 @@ import time
 import datetime
 import os
 import copy
+import json
+
+from suds.client import Client
+import base64
+import zlib
+
 
 class Get_data():
     def __init__(self):
         self.headers = {
-            'Host':"data.cma.gov.cn",
+            'Host':"data.cma.cn",
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language': 'en-US,en;q=0.5',
             'Accept-Encoding': 'gzip, deflate',
-            'Cookie':"PHPSESSID=691mi3b3clbs8quddk4tfdmgc2; userLoginKey=3918efe8092ff7cec78d6bae7d957ec7; trueName=%E7%94%B0%E5%86%9B; userName=D7BAB9E7B4C031BA73D2CB5A9405258A",
             'Connection': 'keep-alive'}
-        self.url='http://data.cma.gov.cn/data/search.html?dataCode=A.0012.0001'
-        date=datetime.datetime.now().strftime("%Y-%m-%d")
-        self.dateS=date+' 00'
-        self.dateE=date+' 23'
+        self.url='http://data.cma.cn/data/search.html?dataCode=A.0012.0001'
+        self.session=requests.session()
+        date=datetime.datetime.now().strftime("%Y-%m-%d ")
+        hour=datetime.datetime.now().strftime("%H")
+        sd_hour=int(hour)-18
+        ed_hour=int(hour)-8
+        if ed_hour>=0:
+            if ed_hour<10:
+                self.dateE=date+'0'+str(ed_hour)
+            else:
+                self.dateE=date+str(ed_hour)
+        else:
+            now_time = datetime.datetime.now()
+            yes_time = now_time + datetime.timedelta(days=-1)
+            yes_time_nyr = yes_time.strftime('%Y-%m-%d ')
+            self.dateE=yes_time_nyr+str(24+ed_hour)
+        if sd_hour>=0:
+            if sd_hour<10:
+                self.dateS=date+'0'+str(sd_hour)
+            else:
+                self.dateS=date+str(sd_hour)
+        else:
+            now_time = datetime.datetime.now()
+            yes_time = now_time + datetime.timedelta(days=-1)
+            yes_time_nyr = yes_time.strftime('%Y-%m-%d ')
+            self.dateS=yes_time_nyr+str(24+sd_hour)
+        print(self.dateS,self.dateE)
         self.failed=[]
-        self.login()
+        statue=False
+        while not statue:
+            statue=self.login()
+            if not statue:
+                print('Login failed')
+            else:
+                print('Login OK')
+
+    def GetStrImg(self):
+        image=open('captcha.png','rb').read()
+        image=base64.b64encode(image)
+        #image=base64.encodestring(zlib.compress(image))
+        client=Client('http://www.qidiansharp.com:9001/Service1.asmx?WSDL')
+        data=client.service.GetStrImg(image.decode())
+        return data
 
     def login(self):
         headers = {
-            'Host':"data.cma.gov.cn",
+            'Host':"data.cma.cn",
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language': 'en-US,en;q=0.5',
             'Accept-Encoding': 'gzip, deflate',
             'Connection': 'keep-alive'}
-        url='http://data.cma.gov.cn/user/Login.html'
-        data={
-        'userName':'aikesubi@gmail.com',
-        'password':'Satpi&b8'
-        }
         try:
-            html=requests.post(url,data=data,headers=headers,timeout=30).text
+            url='http://data.cma.cn/user/toLogin.html'
+            self.session.get(url,headers=headers)
+            html=self.session.get('http://data.cma.cn/user/captcha/refresh/1.html',headers=headers).text
+            imgurl='http://data.cma.cn/'+json.loads(html)['url']
+            with open('captcha.png','wb') as img:
+                imgcontent=self.session.get(imgurl,headers=headers).content
+                img.write(imgcontent)
+            imgstr=self.GetStrImg()
+            data={
+            'userName':'aikesubi@gmail.com',
+            'password':'Satpi&b8',
+            'verifyCode':imgstr
+            }
         except:
-            return
-            
+            return False
+        try:
+            html=self.session.post('http://data.cma.cn/user/Login.html',data=data,headers=headers,timeout=30).text
+            statue=json.loads(html)['status']
+            if statue==100:
+                return True
+            else:
+                return False
+        except:
+            return False
+
     def run(self):
         try:
             os.mkdir('data')
@@ -56,6 +114,7 @@ class Get_data():
     def get_failed(self,failed):
         lists=copy.copy(failed)
         for code in lists:
+            #pass
             self.get_data(code)
 
     def get_data(self,code):
@@ -72,7 +131,7 @@ class Get_data():
         'dataCode':'A.0012.0001',
         'dataCodeInit':'A.0012.0001'}
         try:
-            html=requests.post(self.url,headers=self.headers,data=data,timeout=30).text
+            html=self.session.post(self.url,headers=self.headers,data=data,timeout=30).text
             table=BeautifulSoup(html,'html.parser').find('div',id='divSearchData').find('table').find_all('tr')
         except:
             self.failed.append(code)
@@ -88,8 +147,5 @@ class Get_data():
         print(code+'---ok')
 
 if __name__=='__main__':
-    sleep_time=input('输入间隔时间(小时)：')
-    while True:
-        work=Get_data()
-        work.run()
-        time.sleep(float(sleep_time)*3600)
+    work=Get_data()
+    work.run()
