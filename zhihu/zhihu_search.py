923c7506d20a7b0acdcfc57de91faf2a62ce36a3
Nyloner <nyloner.root@gmail.com>
2016-07-20 10:21:41 +0800

    zhihu

--- zhihu/zhihu_search.py
+++ zhihu/zhihu_search.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-
+import time
 
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -11,41 +11,71 @@ headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0"}
 
 def search(key):
+    result=[]
     offset=0
     while offset<100:
         data=requests.get('https://www.zhihu.com/r/search?q={}&type=content&offset={}'.format(key,offset),headers=headers).text
         items=parser(data)
-        print(items)
-        return
+        for item in items:
+            question=question_infor(item)
+            result.append(question)
+            time.sleep(1)
+        offset+=10
+        print(offset)
+    return result
 
 def parser(data):
     htmls=json.loads(data)['htmls']
     result=[]
     for html in htmls:
         item={}
-        soup=BeautifulSoup(html,'html.parser').find('div',{'class':'content'})
-        item['question-id']=soup.find('meta',{'itemprop':'question-id'}).get('content')
-        item['question-url-token']=soup.find('meta',{'itemprop':'question-url-token'}).get('content')
-        item['title']=soup.find('a').get_text()
-        answer=soup.find('div',{'class':['entry','answer']})
-        item['answer-id']=answer.find('meta',{'itemprop':'answer-id'}).get('content')
-        item['answer-url-token']=answer.find('meta',{'itemprop':'answer-url-token'}).get('content')
-        item['votecount']=answer.find('a',{'class':'zm-item-vote-count'}).get_text()
         try:
-            item['author']=answer.find('a',{'class':'author'}).get_text()
+            soup=BeautifulSoup(html,'html.parser')
+            item['title']=soup.find('a').get_text()
+            soup=soup.find('div',{'class':'content'})
+            item['question-id']=soup.find('meta',{'itemprop':'question-id'}).get('content')
+            item['question-url-token']=soup.find('meta',{'itemprop':'question-url-token'}).get('content')
+            answer=soup.find('div',{'class':['entry','answer']})
+            item['answer-id']=answer.find('meta',{'itemprop':'answer-id'}).get('content')
+            item['answer-url-token']=answer.find('meta',{'itemprop':'answer-url-token'}).get('content')
+            item['votecount']=answer.find('a',{'class':'zm-item-vote-count'}).get_text()
+            try:
+                item['author']=answer.find('a',{'class':'author'}).get_text()
+            except:
+                item['author']='匿名用户'
+            item['answer-content']=answer.find('script',{'class':'content'}).get_text()
+            item['answer-content']=BeautifulSoup(item['answer-content'],'html.parser').get_text()
+            item['date']=answer.find('a',{'class':'time text-muted'}).get('data-tooltip')
+            item['answer-comment-count']=answer.find('a',{'class':'js-toggleCommentBox'}).find('span').get_text()
+            if '添加评论' in item['answer-comment-count']:
+                item['answer-comment-count']=0
+            item['copyright']=answer.find('a',{'class':'js-copyright'}).get_text()
+            result.append(item)
         except:
-            item['author']='匿名用户'
-        item['answer-content']=answer.find('script',{'class':'content'}).get_text()
-        item['answer-content']=BeautifulSoup(item['answer-content'],'html.parser').get_text()
-        item['date']=answer.find('a',{'class':'time text-muted'}).get('data-tooltip')
-        item['answer-comment-count']=answer.find('a',{'class':'js-toggleCommentBox'}).find('span').get_text()
-        if '添加评论' in item['answer-comment-count']:
-            item['answer-comment-count']=0
-        item['copyright']=answer.find('a',{'class':'js-copyright'}).get_text()
-        result.append(item)
+            continue
     return result
 
-def question_infor():
+def question_infor(item):
+    html=requests.get('https://www.zhihu.com/question/%s'%(item['question-url-token']),headers=headers).text
+    soup=BeautifulSoup(html,'html.parser').find('div',{'class':'zu-main-content-inner'})
+    try:
+        item['question-content']=soup.find('div',{'class':'zm-editable-content'}).get_text()
+    except:
+        item['question-content']='-'
+    try:
+        item['comment-count']=soup.find('div',id='zh-question-meta-wrap').find('a',{'name':'addcomment'}).get_text()
+        if '添加评论' in item['comment-count']:
+            item['comment-count']=0
+    except:
+        item['comment-count']='-'
+    html=requests.get('https://www.zhihu.com/node/QuestionCommentListV2?params={"question_id":%s}'%(item['question-id']),headers=headers).text
+    table=BeautifulSoup(html,'html.parser').find_all('div',{'class':'zm-item-comment'})
+    item['comments']=[]
+    for div in table:
+        comment={}
+        comment['comment-id']=div.get('data-id')
+        comment['comment-content']=div.find('div',{'class':'zm-comment-content'}).get_text()
+        item['comments'].append(comment)
+    return item
 
-
-search('key')
+result=search("zhihu")
