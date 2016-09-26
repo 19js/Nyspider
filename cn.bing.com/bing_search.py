027d3fae3a51e709589f2e2d9ac58f49608928be
Nyloner <nyloner.root@gmail.com>
2016-09-26 13:15:20 +0800

    bing_search

--- cn.bing.com/bing_search.py
+++ cn.bing.com/bing_search.py
@@ -12,15 +12,15 @@ headers = {
     'Host':"cn.bing.com",
     "Accept-Language": "en-US,en;q=0.5",
     "Connection": "keep-alive",
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0"}
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/44.0"}
 
 def search(key,need_urls):
-    html=requests.get('http://cn.bing.com/search?q='+key.replace('-',' '),headers=headers).text
+    html=requests.get('http://cn.bing.com/search?q='+key.replace(' ','+')+'&go=Search&qs=bs&form=QBRE',headers=headers).text
     table=BeautifulSoup(html,'lxml').find('ol',id="b_results").find_all('li')
     result=[]
     name=key.lower()
     replace_keys=[',','(',')','...','[',']','?','!','/','*','&','^','%','$'
-                ,'#','@','<','>','.',',','~','`',"'",'_','+','=','？','。','，','；','：']
+                ,'#','@','<','>','.',',','~','`',"'",'_','+','=','？','。','，','；','：','|']
     for punctuation in replace_keys:
         name=name.replace(punctuation,'').replace(' ','')
     names=name.split('-')
@@ -38,6 +38,8 @@ def search(key,need_urls):
         for strong in h2.find_all('strong'):
             str1+=strong.get_text()
         str1=str1.lower().replace(' ','')
+        for punctuation in replace_keys:
+            str1=str1.replace(punctuation,'').replace(' ','')
         status=True
         for key in names:
             if key.replace(' ','') not in str1:
@@ -81,15 +83,25 @@ def main():
             encoding='GBK'
         excel=openpyxl.Workbook(write_only=True)
         sheet=excel.create_sheet()
-        for key in open('data/'+filename,'r',encoding==encoding):
-            key=key.replace('\r','').replace('\n','')
+        for key in open('data/'+filename,'r',encoding=encoding):
+            key=key.replace('\r','').replace('\n','').replace('\t','')
             line=[key]
             if key.replace(' ','').replace('\t','')=='':
                 continue
-            try:
-                result=search(key,need_urls)
-            except:
-                sheet.append(line)
+            count=0
+            while True:
+                try:
+                    result=search(key,need_urls)
+                    if result==[] and count<10:
+                        count+=1
+                        continue
+                    break
+                except:
+                    if count==10:
+                        sheet.append(line+['失败'])
+                        result==False
+                    count+=1
+            if result==False:
                 continue
             for item in result:
                 line.append(item)
@@ -101,5 +113,6 @@ def main():
                 pass
         excel.save('result/'+filename+'.xlsx')
     print("完成")
+
 main()
 time.sleep(60)
