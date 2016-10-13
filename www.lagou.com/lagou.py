ae5931a6bae4e957f451045179fe38f5de922382
Nyloner <nyloner.root@gmail.com>
2016-10-13 17:00:18 +0800

    lagou

--- www.lagou.com/lagou.py
+++ www.lagou.com/lagou.py
@@ -1,9 +1,9 @@
 #coding:utf-8
-
 import requests
 import json
-import xlwt3
 import time
+from write_sql import write2sqlite
+from bs4 import BeautifulSoup
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0',
@@ -12,65 +12,66 @@ headers = {
     'Accept-Encoding': 'gzip, deflate',
     'Connection': 'keep-alive'}
 
-def get_job(keyword,pagenum):
-    js_data=requests.get('http://www.lagou.com/jobs/positionAjax.json?px=new&kd=%s&pn=%s&'%(keyword,pagenum),headers=headers).text
-    data=json.loads(js_data)
-    data=data['content']['result']
+def get_jobs(keyword):
     jobs=[]
-    for item in data:
-        job={}
-        job['postiontype']=item['positionType']
-        job['company']=item['companyShortName']
-        job['salary']=item.get('salary')
-        job['workYear']=item['workYear']
-        job['education']=item['education']
-        job['industryField']=item['industryField']
-        job['companySize']=item['companySize']
-        job['createTime']=item['createTime']
-        job['city']=item['city']
-        job['financeStage']=item['financeStage']
-        jobs.append(job)
+    page=1
+    while True:
+        js_data=requests.get('http://www.lagou.com/jobs/positionAjax.json?px=new&kd=%s&pn=%s&'%(keyword,page),headers=headers).text
+        data=json.loads(js_data)
+        data=data['content']['positionResult']['result']
+        for item in data:
+            job={}
+            job['fromsite']='拉勾'
+            job['id']=item['positionId']
+            job['companyId']=item['companyId']
+            job['positionType']=keyword
+            job['positionName']=item['positionName']
+            job['company']=item['companyFullName']
+            job['salary']=item.get('salary')
+            job['workYear']=item['workYear']
+            job['education']=item['education']
+            job['industryField']=item['industryField']
+            job['companySize']=item['companySize']
+            job['city']=item['city']
+            job['financeStage']=item['financeStage']
+            jobs.append(job)
+        print(page,keyword,'ok')
+        page+=1
+        if page==31:
+            break
+        time.sleep(1)
     return jobs
 
-def excel():
-    excel=xlwt3.Workbook()
-    sheet=excel.add_sheet('sheet')
-    count=0
-    labels=['postiontype','company','salary','workYear','education','industryField','companySize','city','financeStage']
-    page=1
-    while page<300:
-        try:
-            jobs=get_job('',page)
-        except:
-            time.sleep(5)
-            continue
-        for job in jobs:
-            num=0
-            for i in labels:
-                sheet.write(count,num,job[i])
-                num+=1
-            count+=1
-        print(page,count)
-        page+=1
-        time.sleep(2)
-        excel.save('jobs.xls')
+def get_job_des(jobid):
+    url='http://www.lagou.com/jobs/%s.html'%jobid
+    html=requests.get(url,headers=headers,timeout=30).text
+    des=BeautifulSoup(html,'lxml').find('dd',{'class':'job_bt'}).get_text()
+    return des
 
-def write2txt():
-    f=open('job.txt','a')
-    page=1
-    count=0
-    while page<300:
-        try:
-            jobs=get_job('',page)
-        except:
-            time.sleep(5)
-            continue
-        for job in jobs:
-            count+=1
-            f.write(str(job)+'\n')
-        print(page,count)
-        page+=1
-        time.sleep(2)
-    f.close()
+def get_company_rate(companyid):
+    url='http://www.lagou.com/gongsi/%s.html'%(companyid)
+    html=requests.get(url,headers=headers,timeout=30).text
+    rate=BeautifulSoup(html,'lxml').find('div',{'class':'reviews-top'}).find('span',{'class':'score'}).get_text()
+    return rate
 
-write2txt()
+def main():
+    keywords=[line.replace('\n','') for line in open('type.txt','r')]
+    for keyword in keywords:
+        jobs=get_jobs(keyword)
+        result=[]
+        for job in jobs:
+            try:
+                des=get_job_des(job['id'])
+            except:
+                des='-'
+            try:
+                rate=get_company_rate(job['companyId'])
+            except:
+                rate='-'
+            job['jobDes']=des
+            job['rate']=rate
+            result.append(job)
+            time.sleep(1)
+        write2sqlite(result,keyword)
+        print(keyword,'ok')
+main()
