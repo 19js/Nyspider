fdd40c3ec7da6fabac58b5e66199e34c63647700
Nyloner <nyloner.root@gmail.com>
2016-10-19 13:11:39 +0800

    add sohu news

--- news.sohu.com/news.py
+++ news.sohu.com/news.py
@@ -70,8 +70,47 @@ def article(url):
     html=requests.get(url,headers=headers).text.encode('iso-8859-1').decode('gbk','ignore')
     soup=BeautifulSoup(html,'lxml').find('div',{'class':'content-wrapper'})
     title=soup.find('h1').get_text()
-    content=soup.find('div',id='contentText').get_text()
+    table=soup.find('div',id='contentText').find_all('p')
+    content=''
+    for item in table:
+        content+=item.get_text()
     return title,content
+
+def social_news():
+    urls=[]
+    page=10842
+    while True:
+        html=requests.get('http://news.sohu.com/shehuixinwen_%s.shtml'%page,headers=headers).text.encode('iso-8859-1').decode('gbk','ignore')
+        try:
+            table=BeautifulSoup(html,'lxml').find('div',{'class':'new-article'}).find_all('div',{'class':'article'})
+        except:
+            break
+        for item in table:
+            url=item.find_all('a')[-1].get('href')
+            if url in urls:
+                continue
+            urls.append(url)
+        if len(urls)>1200:
+            break
+        page-=1
+    print(len(urls))
+    num=20000
+    newstype='社会'
+    for url in urls:
+        try:
+            title,content=article(url)
+        except:
+            continue
+        try:
+            os.mkdir(newstype)
+        except:
+            pass
+        f=open(newstype+'/%s.txt'%num,'w',encoding='utf-8')
+        f.write(title+'\r\n\r\n'+content)
+        f.close()
+        print(newstype,title)
+        num+=1
+        print(num)
 
 def main():
     num=10000
@@ -94,4 +133,4 @@ def main():
         num+=1
         print(num)
 
-main()
+social_news()
