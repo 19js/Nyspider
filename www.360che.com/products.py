2cac3a0714ae3cc3b64def4fefaaf4913a64f353
Nyloner <nyloner.root@gmail.com>
2016-11-14 10:41:03 +0800

    update 360che

--- www.360che.com/products.py
+++ www.360che.com/products.py
@@ -2,12 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 import openpyxl
 import time
+import pymysql
+import json
 
 headers={
     'Host': 'product.m.360che.com',
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 }
+
+def load_mysql_setting():
+    f=open('./mysql_setting.json','r',encoding='utf8')
+    config=json.load(f)
+    return config
+
+def insert_into_mysql(result):
+    config=load_mysql_setting()
+    conn=pymysql.connect(**config)
+    #conn=pymysql.connect(host=userdata['host'],user=userdata['user'],passwd=userdata['passwd'],db=userdata['db'],port=userdata['port'],charset='utf-8')
+    cur=conn.cursor()
+    try:
+        cur.execute("create table if not exists products(name char(80),series char(80),brand char(80),销售情况 char(80),基本信息 TEXT,发动机 TEXT,货箱参数 TEXT,驾驶室参数 TEXT,变速箱 TEXT,轮胎 TEXT,制动器 TEXT,油箱 TEXT,底盘 TEXT,其它信息 TEXT)")
+    except:
+        pass
+    keys=['brand','series','name','销售情况','基本信息', '发动机', '货箱参数', '驾驶室参数', '变速箱', '轮胎', '制动器', '油箱', '底盘','其它信息']
+    for item in result:
+        line=[]
+        for key in keys:
+            try:
+                line.append(str(item[key]))
+            except:
+                line.append('')
+        cur.execute('insert into products(brand,series,name,销售情况,基本信息, 发动机, 货箱参数, 驾驶室参数, 变速箱, 轮胎, 制动器, 油箱, 底盘,其它信息) values'+str(tuple(line)))
+    conn.commit()
+    cur.close()
 
 def get_brandlist():
     html=requests.get('http://product.m.360che.com/brandlist.html',headers=headers).text
@@ -43,7 +71,7 @@ def get_products(url):
     items=table[0].find_all('li')
     for item in items:
         try:
-            name=item.find('a').get_text()
+            name=item.find('a').get_text().replace('\n','').replace('人气','')
             url=item.find('a').get('href').replace('_index','_param')
         except:
             continue
@@ -54,7 +82,7 @@ def get_products(url):
         items=[]
     for item in items:
         try:
-            name=item.find('a').get_text()
+            name=item.find('a').get_text().replace('\n','').replace('人气','')
             url=item.find('a').get('href').replace('_index','_param')
         except:
             continue
@@ -65,13 +93,11 @@ def product_infor(url):
     html=requests.get('http://product.m.360che.com/'+url,headers=headers,timeout=30).text
     table=BeautifulSoup(html,'lxml').find('table',id='datatable_main').find_all('tr')
     infor={}
-    keys=[]
     title=''
     for item in table:
         class_name=item.get('class')
         if class_name==['title']:
             title=item.get_text().replace('\n','')
-            keys.append(title)
         if class_name!=None:
             continue
         try:
@@ -89,22 +115,36 @@ def product_infor(url):
         except:
             infor[title]={}
             infor[title][key]=value
-    f=open('keys.txt','a')
-    f.write(str(keys)+'\n')
-    f.close()
     return infor
 
 def main():
     brandlist=get_brandlist()
+    keys=['brand','series','name','销售情况','基本信息', '发动机', '货箱参数', '驾驶室参数', '变速箱', '轮胎', '制动器', '油箱', '底盘','其它信息']
     for brand in brandlist:
         series=get_series(brand[1],brand[2])
         for item in series:
             products=get_products(item[1])
+            result=[]
             for product in products:
+                car={}
+                for key in keys:
+                    car[key]={}
+                car['brand']=brand[0]
+                car['series']=item[0]
+                car['name']=product[1]
+                car['销售情况']=product[0]
                 try:
                     infor=product_infor(product[-1])
                 except:
-                    print(product)
-                print(brand[0],item[0],product[1])
-                break
+                    continue
+                for key in infor:
+                    try:
+                        value=car[key]
+                        car[key]=infor[key]
+                    except:
+                        car['其它信息'][key]=infor[key]
+                result.append(car)
+            print(item[0])
+            insert_into_mysql(result)
+
 main()
