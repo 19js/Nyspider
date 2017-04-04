37644db9e469d0924a8923603f585dbc85725ec2
Nyloner <nyloner.root@gmail.com>
2017-04-04 13:12:30 +0800

    add www.globalsources.com

---
+++ www.globalsources.com/globalsources.py
@@ -0,0 +1,218 @@
+import requests
+from bs4 import BeautifulSoup
+import openpyxl
+import time
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr,formataddr
+import smtplib
+
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "keep-alive",
+    'Host':'www.globalsources.com',
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0"}
+
+def load_email():
+    text=open('Email.txt','r',encoding='utf-8').read()
+    text=text.replace('\r','').replace('\n','').replace(' ','').replace('\t','')
+    result=text.split('---')
+    return result
+
+def send_email(fromemail,passwd,toemail,subject,text):
+    msg = MIMEText(text, 'plain', 'utf-8')
+    msg['Subject']=subject
+    msg['From'] = _format_addr(fromemail)
+    msg['To'] = _format_addr(toemail)
+    server=smtplib.SMTP_SSL('smtp.qq.com')
+    server.ehlo('smtp.qq.com')
+    server.login(fromemail,passwd)
+    server.sendmail(fromemail, [toemail], msg.as_string())
+    server.quit()
+
+def get_type_list():
+    url='http://www.globalsources.com/new-products/NL0/Listing.htm'
+    html=requests.get(url,headers=headers,timeout=30).text
+    soup=BeautifulSoup(html,'lxml').find_all('div',{'class':'margintop'})
+    result=[]
+    for item in soup:
+        try:
+            result.append('http://www.globalsources.com/'+item.find('a').get('href'))
+        except:
+            continue
+    return result
+
+def get_allow_company_urls(url):
+    html=requests.get(url,headers=headers,timeout=30).text
+    table=BeautifulSoup(html,'lxml').find_all('div',{'class':'detail_supplierInfo'})
+    result=[]
+    for item in table:
+        try:
+            a=item.find('a',{'class':'detail_name'})
+            title=a.get_text()
+            if 'shenzhen' not in title.lower():
+                continue
+            url=a.get('href')
+            memberSince=item.find('span',{'class':'memberSince'}).find_all('img')
+            if 'num1' in memberSince[0].get('class') and 'st' in memberSince[1].get('class') and 'year' in memberSince[2].get('class'):
+                if [title,url] in result:
+                    continue
+                result.append([title,url])
+        except:
+            continue
+    return result
+
+def get_company_info(url):
+    html=requests.get(url,headers=headers,timeout=30).text
+    table=BeautifulSoup(html,'lxml').find('div',{'class':'VBinformaition'}).find_all('p')
+    company_name=''
+    address=''
+    number=''
+    for item in table:
+        try:
+            text=item.get_text().replace('\r','').replace('\n','').replace('\t','')
+        except:
+            continue
+        if 'Registered Company:' in text:
+            company_name=text.replace('Registered Company:','')
+        if 'Business Registration Number:' in text:
+            number=text.replace('Business Registration Number:','')
+        if 'Company Registration Address:' in text:
+            address=text.replace('Company Registration Address:','')
+    area=''
+    area_keys={'龙岗':['龙岗','坪山新区','坑梓','大鹏新区'],
+                '深圳市区':['南山','罗湖','福田','盐田','前海'],
+                '龙华新区':['龙华新区'],
+                '宝安':['宝安','光明新区','公明']}
+    for key in area_keys:
+        for a_key in area_keys[key]:
+            if a_key in address:
+                area=key
+                break
+        if area!='':
+            break
+    return [company_name,number,address,area]
+
+def get_contact_info(url):
+    html=requests.get(url,headers=headers,timeout=30).text
+    soup=BeautifulSoup(html,'lxml').find('div',{'class':'contactUs'})
+    table=soup.find('div',{'class':'proDetCont'}).find_all('div')
+    need_keys=['Phone Number:','Mobile:','Other Homepage Address:','Homepage Address:','Address:']
+    length=len(need_keys)
+    result=['']*length
+    for item in table:
+        text=item.get_text().replace('\r','').replace('\n','').replace('\t','').replace("&nbsp;",'').replace('\xa0','')
+        for index in range(length):
+            if need_keys[index] in text:
+                result[index]=text.replace(need_keys[index],'')
+                break
+    result[-1],result[-3]=result[-3],result[-1]
+    try:
+        contName=soup.find('p',{'class':'contName'}).get_text().replace('\r','').replace('\n','').replace('\t','').replace('\xa0',' ')
+    except:
+        contName=''
+    result.insert(0,contName)
+    return result
+
+def write_to_txt(result):
+    f=open('temp.txt','a',encoding='utf-8')
+    today=time.strftime("%Y-%m-%d", time.localtime())
+    for line in result:
+        f.write(str([today]+line)+'\r\n')
+    f.close()
+
+def load_today_to_excel():
+    today=time.strftime("%Y-%m-%d", time.localtime())
+    excel=openpyxl.Workbook(write_only=True)
+    sheet=excel.create_sheet()
+    for line in open('temp.txt','r',encoding='utf-8'):
+        try:
+            line=eval(line)
+        except:
+            continue
+        if today in line:
+            try:
+                sheet.append(line)
+            except:
+                continue
+    excel.save('%s.xlsx'%today)
+
+def load_exists():
+    exists={}
+    for line in open('temp.txt','r',encoding='utf-8'):
+        try:
+            line=eval(line)
+        except:
+            continue
+        url=line[-1]
+        exists[url]=1
+    return exists
+
+def crawl():
+    try:
+        sleep_time=input("输入采集频率(S):")
+        sleep_time=int(sleep_time)
+    except:
+        sleep_time=60
+    try:
+        types=get_type_list()
+    except Exception as e:
+        print('获取分类失败',e)
+        return
+
+    try:
+        email_config=load_email()
+    except Exception as e:
+        print('获取Email失败',e)
+        return
+
+    try:
+        exists=load_exists()
+    except:
+        exists={}
+
+    while True:
+        result=[]
+        for type_url in types:
+            try:
+                companys=get_allow_company_urls(type_url)
+            except Exception as e:
+                print('Error[company urls] ',e)
+                continue
+            for company in companys:
+                if company[1] in exists:
+                    continue
+                try:
+                    base_info=get_company_info(company[1])
+                    contact_info=get_contact_info(company[1].replace('Homepage','ContactUs'))
+                except Exception as e:
+                    print('Error[company info] ',e)
+                    continue
+                exists[company[1]]=1
+                print(company[0])
+                result.append([company[0]]+base_info+contact_info+[company[1]])
+        now=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+        print(now,'抓取成功')
+        if len(result)==0:
+            continue
+        write_to_txt(result)
+        text=''
+        for line in result:
+            text+='\r\n'.join(line)+'\r\n'+'-----'*10+'\r\n\r\n'
+        try:
+            send_email(email_config[0], email_config[1], email_config[2],"%s--公司信息"%now, text)
+        except Exception as e:
+            print(now,'Send Email Failed',e)
+        time.sleep(sleep_time)
+
+def globalsources():
+    switch=input("1.导出今天采集数据.\n2.采集.\n")
+    if switch=='1':
+        load_today_to_excel()
+    else:
+        crawl()
+
+globalsources()
