826e7c8e1907d09a42c21e37e3507cb439ecfef5
Nyloner <nyloner.root@gmail.com>
2018-02-23 23:20:58 +0800

    update landchina

--- landchina/landchina.py
+++ landchina/landchina.py
@@ -1,62 +1,74 @@
-#coding:utf-8
+# encoding:utf-8
 
-import requests
+from util import *
 from bs4 import BeautifulSoup
-import openpyxl
-import time
-import os
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate',
-    'Connection': 'keep-alive'}
+import codecs
+import json
 
 
-def get_urls():
-    #province={'山东':'37%','江苏':'32%','浙江':'33%','重庆':'50%','四川':'51%','湖北':'42%','河南':'41%','安徽':'34%','辽宁':'21%'}
-    province={'浙江':'33%','辽宁':'21%'}
-    try:
-        os.mkdir('province')
-    except:
-        pass
-    for key in province:
-        f=open('province/%s.txt'%key,'a',encoding='utf-8')
-        for date in ['2016-1-1~2016-4-7','2016-4-8~2016-9-7']:
-            page=1
-            while True:
-                data={
-                'hidComName':"default",
-                'TAB_QueryConditionItem':'9f2c3acd-0256-4da2-a659-6949c4671a2a',
-                'TAB_QueryConditionItem':"ec9f9d83-914e-4c57-8c8d-2c57185e912a",
-                'TAB_QuerySubmitConditionData':"9f2c3acd-0256-4da2-a659-6949c4671a2a:{}|42ad98ae-c46a-40aa-aacc-c0884036eeaf:{}".format(date,province[key]),
-                'TAB_QuerySubmitOrderData':"",
-                'TAB_RowButtonActionControl':"",
-                'TAB_QuerySubmitPagerData':page,
-                'TAB_QuerySubmitSortData':""
-                }
+def get_item_list(date_limit, province):
+    nodes = {
+        '广东省': '44'
+    }
+    select_district = nodes[province]+'▓~'+province
+    page = 1
+    result = []
+    while True:
+        data = {
+            'hidComName': "default",
+            'TAB_QueryConditionItem': ['9f2c3acd-0256-4da2-a659-6949c4671a2a', 'ec9f9d83-914e-4c57-8c8d-2c57185e912a'],
+            'TAB_QuerySubmitConditionData': "9f2c3acd-0256-4da2-a659-6949c4671a2a:{}|42ad98ae-c46a-40aa-aacc-c0884036eeaf:{}".format(date_limit, select_district.decode('utf-8').encode('GBK')),
+            'TAB_QuerySubmitOrderData': "",
+            'TAB_RowButtonActionControl': "",
+            'TAB_QuerySubmitPagerData': page,
+            'TAB_QuerySubmitSortData': ""
+        }
+        req = build_request(
+            url='http://www.landchina.com/default.aspx?tabid=263', data=data)
+        table = BeautifulSoup(req.text, 'lxml').find(
+            'table', id='TAB_contentTable').find_all('tr')
+        if '没有检索到相关数据' in str(table):
+            break
+        for item in table:
+            try:
+                line=['http://www.landchina.com/'+item.find('a').get('href')]
+            except:
+                continue
+            for td in item.find_all('td'):
                 try:
-                    html=requests.post('http://www.landchina.com/default.aspx?tabid=263',data=data,headers=headers,timeout=30).text
+                    line.append(td.get_text().replace('\r','').replace('\n',''))
                 except:
-                    continue
-                table=BeautifulSoup(html,'lxml').find('table',id='TAB_contentTable').find_all('tr')
-                if '没有检索到相关数据' in str(table):
-                    break
-                for item in table:
-                    try:
-                        line=[item.find('a').get('href')]
-                    except:
-                        continue
-                    for td in item.find_all('td'):
-                        try:
-                            line.append(td.get_text().replace('\r','').replace('\n',''))
-                        except:
-                            line.append('')
-                    f.write(str(line)+'\n')
-                    print(line[-1])
-                print(key,date,page,'ok')
-                page+=1
-        f.close()
+                    line.append('')
+            result.append(line)
+            # try:
+            #     item_url = 'http://www.landchina.com/' + \
+            #         item.find('a').get('href')
+            # except:
+            #     continue
+            # result.append(item_url)
+        print(page, 'OK')
+        if len(table) < 31:
+            break
+        page += 1
+    return result
 
-get_urls()
+def crawl():
+    current_date='2016-01-01'
+    while current_date!='2018-02-23':
+        date='-'.join([str(int(value)) for value in current_date.split('-')])
+        try:
+            result=get_item_list(date+'~'+date,'广东省')
+        except:
+            failed=codecs.open('failed.txt','a',encoding='utf-8')
+            failed.write(current_date+'\n')
+            failed.close()
+            current_date=get_next_date(current_date)
+            continue
+        f=codecs.open('items.txt','a',encoding='utf-8')
+        for item in result:
+            f.write(json.dumps(item)+'\n')
+        f.close()
+        current_date=get_next_date(current_date)
+        print(current_time(),current_date,'OK')
+
+crawl()
