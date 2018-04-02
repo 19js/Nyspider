969050604851d74aa9e4e9605e8bcc6f189bcef0
Nyloner <nyloner.root@gmail.com>
2018-04-02 13:18:27 +0800

    update

--- was.zhuhai.gov.cn/company.py
+++ was.zhuhai.gov.cn/company.py
@@ -63,13 +63,13 @@ def get_company_info(url):
     result = {}
     for index in range(len(values)):
         key = values[index]
-        if key in ['名称', '企业状态', '登记状态', '法定代表人', '成立日期', '核准日期','最近一次核准日期', '商事主体类型', '登记机关']:
+        if key in ['名称', '企业状态', '登记状态', '法定代表人', '成立日期', '核准日期', '最近一次核准日期', '商事主体类型', '登记机关']:
             try:
                 value = values[index+1]
             except:
                 value = ''
             result[key] = value
-        if key == '注册资本':
+        if key == '注册资本' or key == '注册资金':
             try:
                 text = values[index+1]
             except:
@@ -95,10 +95,10 @@ def get_company_info(url):
     except Exception as e:
         address = ''
     try:
-        tyshxydm=re.findall('tyshxydm="(.*?)";',req.text)[0]
+        tyshxydm = re.findall('tyshxydm="(.*?)";', req.text)[0]
     except:
-        tyshxydm=''
-    result['tyshxydm']=tyshxydm
+        tyshxydm = ''
+    result['tyshxydm'] = tyshxydm
     result['address'] = address
     return result
 
@@ -111,58 +111,55 @@ class Info(threading.Thread):
     def run(self):
         self.status = False
         try:
-            self.result=get_company_info(self.base_info[-1])
+            self.result = get_company_info(self.base_info[-1])
         except:
             return
-        self.result['base_info']=self.base_info
-        self.status=True
+        self.result['base_info'] = self.base_info
+        self.status = True
 
 
 def load_items():
-    items=[]
-    for line in open('./files/company_list.txt','r'):
+    items = []
+    for line in open('./files/company_list.txt', 'r'):
         try:
-            item=json.loads(line)
+            item = json.loads(line)
         except:
-            faild=open('./files/failed.txt','a')
+            faild = open('./files/failed.txt', 'a')
             faild.write(line)
             faild.close()
             continue
         items.append(item)
-        if len(items)<5:
+        if len(items) < 5:
             continue
         yield items
-        items=[]
+        items = []
     yield items
 
+
 def crawl():
-    count=0
-    success_count=0
+    count = 0
+    success_count = 0
     for items in load_items():
-        tasks=[]
+        tasks = []
         for item in items:
-            task=Info(item)
+            task = Info(item)
             tasks.append(task)
         for task in tasks:
             task.start()
         for task in tasks:
             task.join()
         for task in tasks:
-            count+=1
+            count += 1
             if task.status:
-                f=open('./files/result.txt','a')
+                f = open('./files/result.txt', 'a')
                 f.write(json.dumps(task.result)+'\n')
                 f.close()
-                success_count+=1
+                success_count += 1
             else:
-                faild=open('./files/failed.txt','a')
+                faild = open('./files/failed.txt', 'a')
                 faild.write(json.dumps(task.base_info)+'\n')
                 faild.close()
-        print(current_time(),count,success_count,'OK')
+        print(current_time(), count, success_count, 'OK')
+
 
 crawl()
-        
-            
-            
-        
-
