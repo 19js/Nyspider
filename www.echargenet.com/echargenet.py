68b57018694550fd5ed3566fa51533092d793a04
Nyloner <nyloner.root@gmail.com>
2018-06-04 20:16:14 +0800

    add echargenet.com

--- www.echargenet.com/echargenet.py
+++ www.echargenet.com/echargenet.py
@@ -1,142 +1,179 @@
 import requests
-import json
-from bs4 import BeautifulSoup
-import openpyxl
 import time
-import chardet
+import openpyxl
+import random
+import datetime
+import json
+import re
+import csv
 import os
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate',
-    'Connection': 'keep-alive'}
 
-def get_station(cityname):
-    data={
-    'chargePointName':cityname,
-    'measuerModel':'',
-    'isAppointment':''
+def get_headers():
+    pc_headers = {
+        "X-Forwarded-For": '%s.%s.%s.%s' % (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
     }
-    html=requests.post('http://www.echargenet.com/portal/mapService/rest/map/queryChargePoint',data=data,headers=headers,timeout=30).text
-    data=json.loads(html)
-    return data
+    return pc_headers
 
-def get_chardet(filename):
-    data=open(filename,'rb').read()
-    coding=chardet.detect(data)
-    return coding['encoding']
 
-def load_cities():
-    encoding=get_chardet('setting/cities.txt')
-    if encoding=='GB2312':
-        encoding='GBK'
-    citynames=[]
-    for line in open('setting/cities.txt'):
-        name=line.replace('\r','').replace('\n','').replace('\t','').replace(' ','')
-        citynames.append(name)
-    return citynames
+class NetWorkError(Exception):
+    pass
 
-def station_infor(stakeName):
-    url='http://www.echargenet.com/portal/mapService/rest/map/queryStakeByStation?stakeName=%s&pageNo='%stakeName
-    page=1
-    try_num=0
-    result=[]
-    while True:
-        if try_num==3:
-            break
+
+def build_request(url, headers=None, data=None, json_data=None, timeout=15, try_times=3):
+    if headers is None:
+        headers = get_headers()
+    for i in range(try_times):
         try:
-            html=requests.get(url+str(page), headers=headers,timeout=30).text
-            try_num=0
-        except:
-            try_num+=1
+            if data:
+                response = requests.post(
+                    url, data=data, headers=headers, timeout=timeout)
+            elif json_data:
+                headers['Content-Type'] = 'application/json'
+                response = requests.post(
+                    url, data=json.dumps(json_data), headers=headers, timeout=timeout)
+            else:
+                response = requests.get(url, headers=headers, timeout=timeout)
+            return response
+        except Exception as e:
             continue
+    raise NetWorkError
+
+
+def write_to_excel(lines, filename, write_only=True):
+    excel = openpyxl.Workbook(write_only=write_only)
+    sheet = excel.create_sheet()
+    for line in lines:
         try:
-            table=BeautifulSoup(html,'html.parser').find('table',attrs={'class':'pop-table'}).find_all('tr')
+            sheet.append(line)
+        except Exception as e:
+            print('Write to excel fail',e)
+            continue
+    excel.save(filename)
+
+
+def write_to_csv(lines, filename):
+    csvfile = open(filename, 'w', encoding='utf-8')
+    spamwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for line in lines:
+        spamwriter.writerow(line)
+    csvfile.close()
+
+
+def get_next_date(current_date='2017-01-01'):
+    current_date = datetime.datetime.strptime(current_date, '%Y-%m-%d')
+    oneday = datetime.timedelta(days=1)
+    next_date = current_date+oneday
+    return str(next_date).split(' ')[0]
+
+
+def current_time():
+    return time.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def load_txt(filename):
+    for line in open(filename, 'r'):
+        try:
+            item = json.loads(line)
+        except Exception as e:
+            print('load txt fail',e)
+            continue
+        yield item
+
+
+def get_all_stations():
+    url = 'http://www.echargenet.com/portal/station/simple'
+    try:
+        req = build_request(url)
+        res_text = re.findall('callback\((.*)\)', req.text)[0]
+        stations = json.loads(res_text)['data']
+    except:
+        return []
+    return stations
+
+
+def get_station_info(id):
+    url = 'http://www.echargenet.com/portal/station/info?id={}'.format(id)
+    try:
+        req = build_request(url)
+        res_text = re.findall('callback\((.*)\)', req.text)[0]
+        station_info = json.loads(res_text)['data']
+    except:
+        return None
+    keys = ['province', 'cityName', 'name', 'address', 'chargerNumDC', 'chargerNumAC',
+            'businessTime', 'parkingFee', 'serviceFee', 'chargingFee', 'payTypeDesc', 'lat', 'lng']
+    result = []
+    for key in keys:
+        try:
+            result.append(station_info[key])
         except:
-            break
-        if len(table)==2:
-            break
-        for tr in table:
-            if 'th class' in str(tr) or 'lastTr' in str(tr):
-                continue
-            tds=tr.find_all('td')
-            line=[]
-            for td in tds:
-                text=td.get_text().replace('\r','').replace('\n','').replace('\t','').replace(' ','')
-                line.append(text)
-            result.append(line)
-        page+=1
+            result.append('')
     return result
 
-def get_time():
-    timenow=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
-    return timenow
 
-def write_to_excel():
-    excel=openpyxl.Workbook(write_only=True)
-    sheet=excel.create_sheet()
-    keys=['date','city','stakeName','stakeTotal','directPile','alternatingPile','stakeAddr','latitude','longitude','parkingPlace','openingHour','feesStandard','chargeServiceFees','manageOrgName','manageContactInfo','序号','名称','类型','状态','最大功率','最大电压','电压','电流','电量','SOC','支付方式','接口标准']
-    sheet.append(keys)
-    for filename in os.listdir(path='.'):
-        if not filename.endswith('.txt'):
-            continue
-        for line in open(filename,'r',encoding='utf-8'):
-            try:
-                sheet.append(eval(line))
-            except:
-                continue
-    excel.save('result/result.xlsx')
-
-def main():
-    citynames=load_cities()
+def get_station_chargers(id):
+    url = 'http://www.echargenet.com/portal/station/chargers?id={}'.format(id)
     try:
-        os.mkdir('result')
+        req = build_request(url)
+        res_text = re.findall('callback\((.*)\)', req.text)[0]
+        chargers_list = json.loads(res_text)['data']
     except:
-        pass
-    keys=['stakeName','stakeTotal','directPile','alternatingPile','stakeAddr','latitude','longitude','parkingPlace','openingHour','feesStandard','chargeServiceFees','manageOrgName','manageContactInfo']
-    for cityname in citynames:
-        count=0
-        status=True
-        while True:
-            try:
-                stations=get_station(cityname=cityname)
-                break
-            except:
-                if count==3:
-                    status=False
-                    break
-                count+=1
-        if status==False:
-            print(get_time(),cityname,'Failed')
-            continue
-        f=open(cityname+'.txt','a',encoding='utf-8')
-        for station in stations:
-            timenow=get_time()
-            line=[timenow,cityname]
+        return []
+    reg_status = {1: "故障", 2: "空闲", 3: "使用", 4: "离线", 5: "使用"}
+    result = []
+    keys = ['area', 'stakeNo', 'chargerType', 'status', 'standardDesc', 'soc',
+            'outPower', 'outVolt', 'current', 'voltage']
+    for chargers in chargers_list:
+        for item in chargers['chargers']:
+            line = []
             for key in keys:
                 try:
-                    line.append(station[key])
-                except:
+                    value = item[key]
+                    if key == 'status':
+                        value = reg_status[value]
+                    line.append(value)
+                except Exception as e:
+
                     line.append('')
-            infor=station_infor(station['stakeName'])
-            for item in infor:
-                f.write(str(line+item)+'\n')
-            try:
-                print(timenow,station['stakeName'],'ok')
-            except:
-                pass
+            result.append(line)
+    return result
+
+
+def crawl():
+    stations = get_all_stations()
+    try:
+        os.mkdir('./files/')
+    except:
+        pass
+    try:
+        os.mkdir('./result')
+    except:
+        pass
+    current_date = current_time().split(' ')[0]
+    filename = './files/'+current_date+'.txt'
+    f = open(filename, 'a', encoding='utf-8')
+    f.write(json.dumps(['province', 'cityName', 'name', 'address', 'chargerNumDC', 'chargerNumAC',
+                        'businessTime', 'parkingFee', 'serviceFee', 'chargingFee', 'payTypeDesc', 'lat', 'lng']+['area', 'stakeNo', 'chargerType', 'status', 'standardDesc', 'soc',
+                                                                                                                 'outPower', 'outVolt', 'current', 'voltage'], ensure_ascii=False)+'\n')
+    f.close()
+    for station in stations:
+        station_id = station['id']
+        station_info = get_station_info(station_id)
+        chargers_list = get_station_chargers(station_id)
+        f = open(filename, 'a', encoding='utf-8')
+        for charge in chargers_list:
+            f.write(json.dumps(station_info+charge, ensure_ascii=False)+'\n')
         f.close()
-        print(get_time(),cityname,'ok')
-    write_to_excel()
-    
-try:
-    sleeptime=input("输入间隔时间(分钟):")
-    sleeptime=int(sleeptime)
-except:
-    sleeptime=15
-while True:
-    main()
-    print("sleep")
-    time.sleep(sleeptime*60)
+        try:
+            print(current_time(), station_info[2])
+        except:
+            pass
+    write_to_excel(load_txt(filename), './result/'+current_date+'.xlsx')
+
+
+crawl()
+input('完成')
