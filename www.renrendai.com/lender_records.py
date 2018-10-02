9316eee4834820573bb0698ca26983c76fac73dc
Nyloner <nyloner.root@gmail.com>
2018-10-02 21:14:11 +0800

    update renrendai

---
+++ www.renrendai.com/lender_records.py
@@ -0,0 +1,102 @@
+# coding:utf-8
+import json
+import codecs
+import csv
+import requests
+import random
+import time
+
+
+def get_headers():
+    pc_headers = {
+        "X-Forwarded-For": '%s.%s.%s.%s' % (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "en-US,en;q=0.5",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
+    }
+    return pc_headers
+
+
+class NetWorkError(Exception):
+    pass
+
+
+def build_request(url, headers=None, data=None):
+    if headers is None:
+        headers = get_headers()
+    for i in range(3):
+        try:
+            if data:
+                response = requests.post(
+                    url, data=data, headers=headers, timeout=30)
+            else:
+                response = requests.get(url, headers=headers, timeout=30)
+            return response
+        except:
+            continue
+    raise NetWorkError
+
+
+def get_lender_records(loan_id):
+    url = 'https://www.renrendai.com/pc/transfer/detail/loanInvestment?loanId={}'.format(
+        loan_id)
+    req = build_request(url)
+    data = req.json()
+    keys = ['loanId', 'userId', 'userNickName',
+            'financeCategory', 'amount', 'lendTime']
+    if 'status' in data and data['status'] == 0:
+        result = []
+        base_info = [data['data']['joinCount'], data['data']['amount']]
+        for lender in data['data']['list']:
+            lender_line = []
+            for key in keys:
+                try:
+                    value = lender[key]
+                    if 'Time' in key:
+                        try:
+                            value = time.strftime(
+                                '%Y-%m-%d %H:%M:%S', time.localtime(int(value)/1000))
+                        except Exception as e:
+                            print(e)
+                    lender_line.append(value)
+                except:
+                    lender_line.append('')
+            result.append(base_info+lender_line)
+        return result
+    return None
+
+
+def append_to_csv_file(lines, filename):
+    csvfile = codecs.open(filename, 'a', encoding='utf-8')
+    spamwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+    for line in lines:
+        spamwriter.writerow(line)
+    csvfile.close()
+
+
+def crawl():
+    loan_id = 2121001
+    loan_id_to = 2486529
+    while loan_id < loan_id_to:
+        try:
+            result = get_lender_records(loan_id)
+        except Exception as e:
+            print('Fail', loan_id)
+            continue
+        if result is None:
+            print('Fail', loan_id)
+            continue
+        append_to_csv_file(result, './files/人人贷散标数据-投标记录.csv')
+        print(loan_id, 'OK')
+        loan_id += 1
+
+
+if __name__ == '__main__':
+    try:
+        import os
+        os.mkdir('files')
+    except:
+        pass
+    crawl()
--- www.renrendai.com/renrendai.py
+++ www.renrendai.com/renrendai.py
@@ -1,181 +1,144 @@
 # coding:utf-8
 
-from bs4 import BeautifulSoup
 import requests
-import time
-import xlwt3
+from bs4 import BeautifulSoup
+import json
+import codecs
+import logging
 import re
-import threading
+import random
+import time
+import csv
+import os
 
-class Get_ids():
-    def __init__(self,url):
-        self.session=requests.session()
-        self.url=url
-        self.headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate',
-            'Connection': 'keep-alive'}
-    def run(self):
-        html=self.session.get(self.url,headers=self.headers).text
-        ID_re='loanId":(\d+)'
-        ids=re.findall(re.compile(ID_re),html)
-        return ids
 
-class Get_infor(threading.Thread):
-    def __init__(self,loanid):
-        super(Get_infor,self).__init__()
-        self.loanid=loanid
-        self.url='http://www.renrendai.com/lend/detailPage.action?loanId='+self.loanid
-        self.headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate',
-            'Connection': 'keep-alive'}
+# 用户名
+j_username = ""
+# 密码
+j_password = ''
 
-    def run(self):
-        self.num=1
+Cookies = {}
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate',
+    'Connection': 'keep-alive'}
+
+
+def parser_loan(item):
+    info_keys = ['officeScale', 'carLoan', 'realName', 'creditLevel', 'office', 'province', 'officeType', 'idNo', 'sumCreditPoint', 'houseLoan', 'marriage', 'userId', 'city', 'position',
+                 'university', 'workYears', 'birthDay', 'graduation', 'homeTown', 'carYear', 'hasHouse', 'gender', 'hasCar', 'officeDomain', 'availableCredits', 'promotion', 'nickName', 'salary', 'carBrand']
+    loan_keys = ['loanId', 'title', 'amount', 'status', 'interest', 'repayType', 'verifyState', 'borrowerLevel',
+                 'borrowType', 'months', 'leftMonths', 'description', 'startTime', 'passTime', 'nickName', 'address', 'jobType']
+    detail_keys = ['totalCount', 'successCount', 'alreadyPayCount',
+                   'borrowAmount', 'notPayTotalAmount', 'overdueAmount', 'overdueCount']
+    credit_keys = ['identification', 'borrowStudy',
+                   'incomeDuty', 'credit', 'identificationScanning', 'work']
+    info = []
+    for key in loan_keys:
         try:
-            html=requests.get(self.url,headers=self.headers).text
-            infor_re='"creditInfo":(.*?),"creditPassedTime"'
-            data=re.findall(re.compile(infor_re),html)[0]
-            self.infor=eval(data)
+            value = item['info']['loan'][key]
+            if 'Time' in key:
+                try:
+                    value = time.strftime(
+                        '%Y-%m-%d %H:%M:%S', time.localtime(int(value)/1000))
+                except Exception as e:
+                    print(e)
+            info.append(str(value))
         except:
-            self.num=0
-
-class Main():
-    def __init__(self):
-        self.f=xlwt3.Workbook(encoding='utf-8')
-        self.sheet=self.f.add_sheet('sheet',cell_overwrite_ok=True)
-        self.count=1
-        head=['实地认证','身份认证','信用报告','工作认证','收入认证','房产认证','购车认证','结婚认证','学历认证','手机实名认证','微博认证','居住地证明']
-        self.sheet.write(0,0,'ID')
-        num=1
-        for i in head:
-            self.sheet.write(0,num,i)
-            num+=1
-        self.f.save('data.xls')
-    def run(self):
-        startID=input('输入起始ID:')
-        endID=input('输入结束ID:')
-        startID=int(startID)
-        endID=int(endID)
-        ids=[]
-        threads=[]
-        while startID<endID:
-            ids.append(startID)
-            if len(ids)<10:
-                startID+=1
-                continue
-            for ID in ids:
-                work=Get_infor(str(ID))
-                threads.append(work)
-            for thread in threads:
-                thread.start()
-            for thread in threads:
-                thread.join()
-            for thread in threads:
-                if thread.num==0:
-                    continue
-                self.sheet.write(self.count,0,thread.loanid)
-
-                if thread.infor['fieldAudit']=='VALID':
-                    self.sheet.write(self.count,1,'2')
-                elif thread.infor['fieldAudit']=='INVALID':
-                    self.sheet.write(self.count,1,'0')
-                else:
-                    self.sheet.write(self.count,1,'1')
-
-                if thread.infor['identification']=='VALID':
-                    self.sheet.write(self.count,2,'2')
-                elif thread.infor['identification']=='INVALID':
-                    self.sheet.write(self.count,2,'0')
-                else:
-                    self.sheet.write(self.count,2,'1')
-
-                if thread.infor['credit']=='VALID':
-                    self.sheet.write(self.count,3,'2')
-                elif thread.infor['credit']=='INVALID':
-                    self.sheet.write(self.count,3,'0')
-                else:
-                    self.sheet.write(self.count,3,'1')
-
-                if thread.infor['work']=='VALID':
-                    self.sheet.write(self.count,4,'2')
-                elif thread.infor['work']=='INVALID':
-                    self.sheet.write(self.count,4,'0')
-                else:
-                    self.sheet.write(self.count,4,'1')
-
-                if thread.infor['incomeDuty']=='VALID':
-                    self.sheet.write(self.count,5,'2')
-                elif thread.infor['incomeDuty']=='INVALID':
-                    self.sheet.write(self.count,5,'0')
-                else:
-                    self.sheet.write(self.count,5,'1')
-
-                if thread.infor['house']=='VALID':
-                    self.sheet.write(self.count,6,'2')
-                elif thread.infor['house']=='INVALID':
-                    self.sheet.write(self.count,6,'0')
-                else:
-                    self.sheet.write(self.count,6,'1')
-
-                if thread.infor['car']=='VALID':
-                    self.sheet.write(self.count,7,'2')
-                elif thread.infor['car']=='INVALID':
-                    self.sheet.write(self.count,7,'0')
-                else:
-                    self.sheet.write(self.count,7,'1')
-
-                if thread.infor['marriage']=='VALID':
-                    self.sheet.write(self.count,8,'2')
-                elif thread.infor['marriage']=='INVALID':
-                    self.sheet.write(self.count,8,'0')
-                else:
-                    self.sheet.write(self.count,8,'1')
-
-                if thread.infor['graduation']=='VALID':
-                    self.sheet.write(self.count,9,'2')
-                elif thread.infor['graduation']=='INVALID':
-                    self.sheet.write(self.count,9,'0')
-                else:
-                    self.sheet.write(self.count,9,'1')
+            info.append('')
+    for key in info_keys:
+        try:
+            info.append(str(item['info']['borrower'][key]))
+        except:
+            info.append('')
+    for key in detail_keys:
+        try:
+            info.append(str(item['info']['userLoanRecord'][key]))
+        except:
+            info.append('')
+    for key in credit_keys:
+        try:
+            info.append(str(item['info']['creditInfo'][key]))
+        except:
+            info.append('')
+    return info
 
 
-                if thread.infor['mobileReceipt']=='VALID':
-                    self.sheet.write(self.count,10,'2')
-                elif thread.infor['mobileReceipt']=='INVALID':
-                    self.sheet.write(self.count,10,'0')
-                else:
-                    self.sheet.write(self.count,10,'1')
+def append_to_csv_file(lines, filename):
+    csvfile = codecs.open(filename, 'a', encoding='utf-8')
+    spamwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+    for line in lines:
+        spamwriter.writerow(line)
+    csvfile.close()
 
-                if thread.infor['kaixin']=='VALID':
-                    self.sheet.write(self.count,11,'2')
-                elif thread.infor['kaixin']=='INVALID':
-                    self.sheet.write(self.count,11,'0')
-                else:
-                    self.sheet.write(self.count,11,'1')
 
-                if thread.infor['residence']=='VALID':
-                    self.sheet.write(self.count,12,'2')
-                elif thread.infor['residence']=='INVALID':
-                    self.sheet.write(self.count,12,'0')
-                else:
-                    self.sheet.write(self.count,12,'1')
-                self.count+=1
-                print(self.count)
-            time.sleep(5)
-            ids=[]
-            threads=[]
-            startID+=1
-            self.f.save('data.xls')
+def login():
+    data = {
+        'j_username': j_username,
+        'j_password': j_password,
+        'rememberme': 'on',
+        'targetUrl': 'https://www.renrendai.com/loan.html',
+        'returnUrl': ''}
+    session = requests.session()
+    html = session.post(
+        'https://www.renrendai.com/passport/index/doLogin', data=data, headers=headers)
+    global Cookies
+    Cookies = session.cookies.get_dict()
+    print('login', Cookies)
 
-def test():
-    work=Main()
 
-if __name__=='__main__':
-    work=Main()
-    work.run()
+def get_loan_info(loan_id):
+    url = 'https://www.renrendai.com/loan-{}.html'.format(loan_id)
+    html = requests.get(url, cookies=Cookies, headers=headers).text
+    detail = re.findall("var detail = '(.*?)';", html)[0]
+    data = json.loads('"%s"' % detail)
+    detail = json.loads(data)
+    info = re.findall("var info = '(.*?)';", html)[0]
+    data = json.loads('"%s"' % info)
+    info = json.loads(data)
+    return {
+        'detail': detail,
+        'info': info
+    }
+
+
+def crawl():
+    loan_id = 2514193
+    loan_id_to = 2714193
+    login()
+    temp_lines = []
+
+    while loan_id < loan_id_to:
+        try:
+            loan_info = get_loan_info(loan_id)
+            line = parser_loan(loan_info)
+            line.append(json.dumps(loan_info))
+        except Exception as e:
+            print('Error', e)
+            try:
+                login()
+            except:
+                pass
+            continue
+
+        temp_lines.append(line)
+        # 20条保存一次
+        if len(temp_lines) == 20:
+            append_to_csv_file(temp_lines, './files/人人贷散标数据.csv')
+            temp_lines = []
+
+        print(loan_id, 'OK')
+        loan_id += 1
+    append_to_csv_file(temp_lines, './files/人人贷散标数据.csv')
+
+
+if __name__ == '__main__':
+    try:
+        os.mkdir('files')
+    except:
+        pass
+    crawl()
